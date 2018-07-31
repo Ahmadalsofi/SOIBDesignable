713f7e41b3da46bb5ba85f5dd4f4c2acc2cdffe4
“ahmad.alsofi” <ahmad.alsofi@erabia.com>
2018-07-31 10:21:45 +0300

    first commit

---
+++ SOIBDesignable/SOIBDesignable.swift
@@ -0,0 +1,137 @@
+//
+//  SOIBDesignable.swift
+//  SOIBDesignable
+//
+//  Created by ahmad alsofi on 7/31/18.
+//  Copyright © 2018 AhmadAlsofi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class SODesignableView: UIView {
+}
+
+@IBDesignable
+class SODesignableTextField: UITextField {
+}
+
+@IBDesignable
+class SODesignableCollectionViewCell: UICollectionViewCell {
+}
+
+@IBDesignable
+class SODesignableTableViewCell: UITableViewCell {
+}
+
+@IBDesignable
+class SODesignableLabel: UILabel {
+    @IBInspectable
+    var cornerRadiusLbl: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
+@IBDesignable
+class SODesignableTableView: UITableView {
+    
+}
+
+@IBDesignable
+class SODesignableButton: UIButton {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+           // layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
